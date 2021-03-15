945f620b505738d60a429d8a4d361f1f08098ded
Jeff Bellucci <jeff@curvehealth.com>
2021-03-15 12:30:41 -0700

    first commit, add sass

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+export const HomePage = props => (
+    <div className = 'homepage'>
+        <div className = 'directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'> Mens</h1>
+                <span className='title'>SHOP Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+)
