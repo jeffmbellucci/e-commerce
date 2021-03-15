b7efb5f1ebc7462e6392ab51a22ef652d06c26a0
Jeff Bellucci <jeff@curvehealth.com>
2021-03-15 13:25:06 -0700

    changes from lesson 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
-import { HomePage } from './homepage.component';
 
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+    <p>what</p>
+  </div>
+);
+
+export default HomePage;
