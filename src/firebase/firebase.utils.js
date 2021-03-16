ba47e9e736a72d5a0afcdf756611e736240738d2
Jeff Bellucci <jeff@curvehealth.com>
2021-03-16 15:21:07 -0700

    add email and username signup

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,15 +3,40 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyAXhgSWEmCLHQUjsO-ji6Cs1OqvS9w-xBg",
-  authDomain: "e-commerce-jmb.firebaseapp.com",
-  projectId: "e-commerce-jmb",
-  storageBucket: "e-commerce-jmb.appspot.com",
-  messagingSenderId: "718224877757",
-  appId: "1:718224877757:web:e779a736781fa53b6e2fd4"
+    apiKey: "AIzaSyAXhgSWEmCLHQUjsO-ji6Cs1OqvS9w-xBg",
+    authDomain: "e-commerce-jmb.firebaseapp.com",
+    projectId: "e-commerce-jmb",
+    storageBucket: "e-commerce-jmb.appspot.com",
+    messagingSenderId: "718224877757",
+    appId: "1:718224877757:web:e779a736781fa53b6e2fd4"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
